bb10ba9f83c2a4257f808f2975557d8308e0c15d
Jared Manning <Jared@Jareds-MacBook-Pro-7.local>
2019-09-24 18:11:29 -0700

    skeletoned decision_trees.py, created driver.py with her example code for testing

---
+++ decision_trees.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+#function that takes training data as input. The labels and the features are binary, but the feature vectors can be of
+#any finite dimension. The training feature data (X) should be structured as a 2D numpy array, with each
+#row corresponding to a single sample. The training labels (Y) should be structured as a 2D numpy array,
+#with each row corresponding to a single label. X and Y should have the same number of rows, with each row
+#corresponding to a single sample and its label. max depth is an integer that indicates the maximum depth
+#for the resulting decision tree. DT train binary(X,Y,max depth) should return the decision tree trained
+#using information gain, limited by some given maximum depth. If max depth is set to -1 then learning only
+#stops when we run out of features of our information gain is 0. You may store a decision tree however you
+#would like, i.e. a list of lists, a class, a dictionary, etc.
+def DT_train_binary(X,Y,max_depth):
+	return dt
+
+#function that takes test data X and test labels Y and a learned decision tree model DT, and returns the accuracy on
+#the test data using the decision tree for predictions
+def DT_test_binary(X,Y,DT):
+	accuracy = 0
+	return accuracy
+
+#Generate decision trees based on information gain on the training data, and return the tree that gives the
+#best accuracy on the validation data.
+def DT_train_binary_best(X_train, Y_train, X_val, Y_val):
+	return dt
+
+#This function should take a single sample and a trained decision tree and return a single classification.
+#The output should be a scalar value. You will use this function with the three trees to generate three
+#predictions per sample and use a majority vote to make a final decision. Present your results and accuracy
+#on the test data presented below.
+def DT_make_prediction(x,DT):
+	prediction = 0
+	return prediction
+
+
+
+#These functions are defined similarly to those above except that the features are now real values. The labels
+#are still binary. Your decision tree will need to use questions with inequalities: >, ≥, <, ≤. THINK
+#ABOUT HOW THIS CAN BE DONE EFFICIENTLY.
+def DT_train_real(X,Y,max_depth):
+	return dt
+
+def DT_test_real(X,Y,DT):
+	accuracy = 0
+	return accuracy
+
+def DT_train_real_best(X_train,Y_train,X_val,Y_val):
+	return dt
---
+++ driver.py
@@ -0,0 +1,7 @@
+import numpy as np
+import decision_trees as dt
+X = np.array([[0,1,0,1],[1,1,1,1],[0,0,0,1]])
+Y = np.array([[1],[1],[0]])
+max_depth = 2
+DT = dt.DT_train_binary(X,Y, max_depth)
+test_acc = dt.DT_test_binary(X,Y,DT)
